e70019b1401cd0213454237793da11073be12bd6
jelly <chm073@gmail.com>
2020-03-23 22:45:48 +0900

    Add field

--- scouter/models.py
+++ scouter/models.py
@@ -3,7 +3,12 @@ from django_extensions.db.models import TimeStampedModel
 
 
 class Problem(TimeStampedModel):
-    problem_number = models.IntegerField()
+    problem_number = models.IntegerField(unique=True)
     title = models.CharField(max_length=180)
+    slug = models.CharField(max_length=180)
     likes = models.IntegerField()
     dislikes = models.IntegerField()
+    difficulty = models.IntegerField()
+    accepted = models.IntegerField()
+    submitted = models.IntegerField()
+
